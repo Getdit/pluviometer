a29221e9e0ed340973714dd29aaaf9ce50940cfb
Erik Na <erikgnaa@gmail.com>
2023-04-17 13:27:55 -0300

    comment mqt

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-from mqtt.utils import client
+# from django.core.wsgi import get_wsgi_application
+# from mqtt.utils import client
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
 
-client.loop_start()
+# client.loop_start()
