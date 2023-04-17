68f5ca27e9bc6966afb1fd15ff4203590b4845ab
Erik Na <erikgnaa@gmail.com>
2023-04-17 19:06:09 -0300

    add: more charts

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from dashboard.views.chart_view import ChartView
 from .views import DashboardView, ReportsView, SensorsView
 
 app_name = "dashboard"
@@ -7,4 +9,5 @@ urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
     path('reports/', ReportsView.as_view(), name='reports'),
     path('sensors/', SensorsView.as_view(), name='sensors'),
+    path('chart/', ChartView.as_view(), name='chart'),
 ]
---
+++ dashboard/views/chart_view.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import View
+from django import forms
+
+class ChartView(View):
+    template_name = 'chart/index.html'
+
+    def get(self, request):
+        labels = ["January", "February", "March", "April", "May", "June", "July"]
+        data = [65, 59, 80, 81, 56, 55, 40]
+
+        # context = super().get_context_data(**kwargs)
+        # context['labels'] = labels
+        # context['data'] = data
+
+        return render(request, self.template_name)
