a15d5ed60c80fc69d9f71686c972ca30f5c8523c
Johny <johnymonteiiro@gmail.com>
2023-05-19 03:30:28 -0300

    craindo a pagina de login

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import DashboardView, ReportsView, SensorsView, MapView
+from .views import DashboardView, ReportsView, SensorsView, MapView,LoginView,ProjectsView
 
 app_name = "dashboard"
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),
+    path('',  MapView.as_view(), name='map'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('account/',  LoginView.as_view(), name='account'),
     path('reports/', ReportsView.as_view(), name='reports'),
     path('sensors/', SensorsView.as_view(), name='sensors'),
     path('map/',  MapView.as_view(), name='map'),
+    path('projects/',  ProjectsView.as_view(), name='projects'),
 ]
