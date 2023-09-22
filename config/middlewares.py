59455d09d7def2a2710ece81d8838716f6a7fe3c
digitalligit@gmail.com <G17hub@)@!>
2023-09-21 22:26:44 -0300

    Fix account login

--- config/middlewares.py
+++ config/middlewares.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
 from accounts.models import Profile
+from django.contrib.auth.models import RelatedObjectDoesNotExist
 
 def verifications_middleware(get_response):
 
@@ -7,17 +8,12 @@ def verifications_middleware(get_response):
         """Forcing the profile creation - This must be changed (for a redirect) when be useful that the user type more data for his profile"""
         user = request.user
         response = get_response(request)
-
-        if not user.profile:
-            Profile.objects.create(
-                user=user
-            )
+        if not Profile.objects.filter(owner=user).exists():
+            Profile.objects.create(owner=user)
 
         if user.is_anonymous == False and not user.is_superuser:
-
-            if ((not user.profile.projects) or user.profile.is_researcher):
+            if (not user.profile.active) or ((not user.profile.projects) or user.profile.is_researcher):
                 return HttpResponse('Opa, seu acesso não foi liberado.')
-
         return response
 
     return middleware
