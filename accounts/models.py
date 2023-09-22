10d4c44e2418b17b001e1bebe406c12680812128
digitalligit@gmail.com <G17hub@)@!>
2023-09-21 22:17:49 -0300

    Fix account login

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Profile(models.Model):
-    phone = models.CharField(max_length=16, verbose_name="Telefone")
+    phone = models.CharField(max_length=16, verbose_name="Telefone", null=True, blank=True)
 
     is_researcher = models.BooleanField(default=False, verbose_name="Pesquisador")
-    research_institution = models.CharField(max_length=100, verbose_name="Instituição de pesquisa")
-    reasearch_area = models.CharField(max_length=150, verbose_name="Área de pesquisa")
+    research_institution = models.CharField(max_length=100, verbose_name="Instituição de pesquisa", null=True, blank=True)
+    reasearch_area = models.CharField(max_length=150, verbose_name="Área de pesquisa", null=True, blank=True)
 
     projects = models.ManyToManyField('core.Project', verbose_name="Projetos (como bolsista)", blank=True, related_name="users")
 
--- config/middlewares.py
+++ config/middlewares.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-
+from accounts.models import Profile
 
 def verifications_middleware(get_response):
 
@@ -7,6 +7,11 @@ def verifications_middleware(get_response):
         """Forcing the profile creation - This must be changed (for a redirect) when be useful that the user type more data for his profile"""
         user = request.user
         response = get_response(request)
+
+        if not user.profile:
+            Profile.objects.create(
+                user=user
+            )
 
         if user.is_anonymous == False and not user.is_superuser:
 
