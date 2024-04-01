f21f235d4c578053df1ae596395dcd66337dcb21
Eduardo_Chedid <G17hub@)@!>
2024-03-31 22:29:59 -0300

    UPDATE: Added project chart gen

--- dashboard/views/project.py
+++ dashboard/views/project.py
@@ -1,8 +1,12 @@
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
+from django.utils import timezone
+from django.utils.html import escapejs
 
-from core.models import Project, DataModel
+from plotly.graph_objs import go
+
+from core.models import Project, DataModel, DeviceLog
 
 class ProjectListView(LoginRequiredMixin, ListView):
     template_name = 'dashboard/projects.html'
@@ -69,3 +73,38 @@ class ProjectDeleteView(LoginRequiredMixin, DeleteView):
 class ProjectChartFormView(LoginRequiredMixin, DetailView):
     template_name = 'dashboard/chart-form.html'
     model = Project
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        data = self.request.GET.get('data').split('_')
+        start_date = timezone.datetime.strptime(self.request.GET.get('start_date'), "%Y-%m-%d")
+        end_date = timezone.datetime.strptime(self.request.GET.get('end_date'), "%Y-%m-%d")
+
+        traces = []
+        labels = []
+
+        for item in data:
+
+            device_id, model_id, type_code =  map(int, item.split(';'))
+            model = DataModel.objects.get(pk=model_id)
+            context[model.name] = model
+            y_values = []
+            x_values = []
+
+            for log in DeviceLog.objects.filter(device_id=device_id, created_at__gte=start_date, created_at__lte=end_date):
+                for dl in log.datalog_set.filter(model=model):
+                    y_values.append(dl.value)
+                    x_values.append(log.created_at)
+            labels.append(model.name)
+            traces.append(go.Scatter(x=x_values, y=y_values, text="symbol", name=model.name))
+
+        layout = go.Layout(title=f"Gráfico gerado: {', '.join(labels)}",
+                           xaxis_title="Data", yaxis_title="")
+
+        # Criar a figura do gráfico com base no traço e layout criados anteriormente
+        fig = go.Figure(data=traces, layout=layout)
+
+        # Converter a figura para JSON para ser exibida na página da web
+        context['main_graph'] = escapejs(fig.to_json())
+        return context
