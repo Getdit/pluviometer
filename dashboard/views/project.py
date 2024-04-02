2bdac6999a25d6f67639d0bed4f1c89deb152c53
Eduardo_Chedid <G17hub@)@!>
2024-04-01 23:21:20 -0300

    UPDATE: Added project chart gen

--- dashboard/views/project.py
+++ dashboard/views/project.py
@@ -127,7 +127,6 @@ class ProjectChartFormView(LoginRequiredMixin, DetailView):
             print(f"Filtered data: {device_data.count()}")
 
             for log in device_data:
-                print(f"Log: {log} DATA: {log.datalog_set.filter(model=model).count()}")
                 for dl in log.datalog_set.filter(model=model):
                     y_values.append(dl.value)
                     x_values.append(log.created_at)
